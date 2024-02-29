0f3c690583cdd593695d97d5708e999b43b88671
OkelloSam21 <samuelokello929@gmail.com>
2024-02-29 04:59:19 +0300

    enhance sign in screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+//    implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -72,7 +72,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //extended icons
-    implementation("androidx.compose.material:material-icons-extended:1.6.2")
+    implementation("androidx.compose.material:material-icons-extended-android:1.6.2")
 
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
--- app/src/main/java/com/samuelokello/kazihub/presentation/shared/authentication/sign_in/SignInScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.samuelokello.kazihub.presentation.shared.authentication.sign_in
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SigInScreen() {
-    
-}
