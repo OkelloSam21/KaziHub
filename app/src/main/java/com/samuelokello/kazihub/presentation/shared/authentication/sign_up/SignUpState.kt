23d213739abb61f9a41e79a3b380e63644788402
OkelloSam21 <samuelokello929@gmail.com>
2024-04-19 12:49:56 +0300

    enhancement

--- app/src/main/java/com/samuelokello/kazihub/presentation/shared/authentication/sign_up/SignUpState.kt
+++ app/src/main/java/com/samuelokello/kazihub/presentation/shared/authentication/sign_up/SignUpState.kt
@@ -1,15 +1,13 @@
 package com.samuelokello.kazihub.presentation.shared.authentication.sign_up
 
-import com.samuelokello.kazihub.presentation.shared.authentication.common.AuthState
+import com.samuelokello.kazihub.presentation.common.AuthState
 
 data class SignUpState (
-//    val isSignUpSuccessful: Boolean = false,
     val signUpError: String? = null,
     val signUpSuccess: Boolean = false,
     val userName: String = "",
     val firstName: String = "",
     val lastName: String = "",
-    val role: String = "",
     val password: String = "",
     val navigateToHome: Boolean = false,
     override val isLoading: Boolean = false,
