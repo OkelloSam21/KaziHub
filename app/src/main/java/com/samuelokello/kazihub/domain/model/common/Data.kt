96cc78a3f1bc269f61d5c94ece3b7a3a07aceb34
OkelloSam21 <samuelokello929@gmail.com>
2024-04-19 15:46:03 +0300

    enhancement

---
+++ app/src/main/java/com/samuelokello/kazihub/domain/model/common/Data.kt
@@ -0,0 +1,31 @@
+package com.samuelokello.kazihub.domain.model.common
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Data(
+    @SerializedName("bio")
+    val bio: String?,
+    @SerializedName("email")
+    val email: String?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("img")
+    val img: Any?,
+    @SerializedName("is_verified")
+    val isVerified: Boolean?,
+    @SerializedName("joined_on")
+    val joinedOn: String?,
+    @SerializedName("lat")
+    val lat: Int?,
+    @SerializedName("location")
+    val location: String?,
+    @SerializedName("lon")
+    val lon: Int?,
+    @SerializedName("phone")
+    val phone: String?,
+    @SerializedName("user_id")
+    val userId: Int?,
+    @SerializedName("verification_code")
+    val verificationCode: Any?
+)
