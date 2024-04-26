6f491392a4367f010da0f58e7b135adcec38ad9f
OkelloSam21 <samuelokello929@gmail.com>
2024-04-27 01:22:34 +0300

    Implemented job-related functionalities in KaziHubRepository
    Added methods for fetching all jobs, fetching a job by ID, fetching job categories, fetching a job category by ID, fetching jobs by location, category, and business. Also added methods for creating a job, creating a job category, updating a job, deleting a job, searching jobs, and filtering jobs by category, business, location, and recency

--- app/src/main/java/com/samuelokello/kazihub/data/repository/KaziHubRepository.kt
+++ app/src/main/java/com/samuelokello/kazihub/data/repository/KaziHubRepository.kt
@@ -2,10 +2,17 @@ package com.samuelokello.kazihub.data.repository
 
 import android.content.Context
 import android.util.Log
+import com.google.android.gms.maps.model.LatLng
 import com.samuelokello.kazihub.data.model.sign_in.SignInResponse
 import com.samuelokello.kazihub.data.remote.KaziHubApi
 import com.samuelokello.kazihub.domain.model.Bussiness.BusinessProfileRequest
 import com.samuelokello.kazihub.domain.model.Bussiness.BusinessProfileResponse
+import com.samuelokello.kazihub.domain.model.job.JobResponse
+import com.samuelokello.kazihub.domain.model.job.category.CategoryResponse
+import com.samuelokello.kazihub.domain.model.job.category.create.CreateCategoryRequest
+import com.samuelokello.kazihub.domain.model.job.category.create.CreateCategoryResponse
+import com.samuelokello.kazihub.domain.model.job.create.CreateJobRequest
+import com.samuelokello.kazihub.domain.model.job.create.CreateJobsResponse
 import com.samuelokello.kazihub.domain.model.shared.auth.sign_in.SignInRequest
 import com.samuelokello.kazihub.domain.model.sign_up.SignUpRequest
 import com.samuelokello.kazihub.domain.model.sign_up.SignUpResponse
@@ -195,6 +202,154 @@ class KaziHubRepository
         }
     }
 
+    // Jobs
+    suspend fun fetchAllJobs(): Resource<List<JobResponse>> {
+        return try {
+            val response = api.getJobs()
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun fetchJobById(id: Int): Resource<JobResponse> {
+        return try {
+            val response = api.getJobById(id = id)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun fetchJobCategories(): Resource<List<CategoryResponse>> {
+        return try {
+            val response = api.getJobCategories()
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun fetchJobCategoryById(id: Int): Resource<CategoryResponse> {
+        return try {
+            val response = api.getJobCategoryById(id = id)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun fetchJobsByLocation(latLng: LatLng): Resource<List<JobResponse>> {
+        return try {
+            val response = api.getJobsNearby(lon = latLng.longitude, lat = latLng.latitude)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun fetchJobsByCategory(categoryID: Int): Resource<List<JobResponse>> {
+        return try {
+            val response = api.getJobsByCategory(id = categoryID)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun fetchJobsByBusiness(businessId: Int): Resource<List<JobResponse>> {
+        return try {
+            val response = api.getJobsByBusiness(id = businessId)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun createJob(request: CreateJobRequest): Resource<CreateJobsResponse> {
+        return try {
+            val token = getAccessToken(context)
+            val response = api.createJob(token, request)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun createJobCategory(request: CreateCategoryRequest): Resource<CreateCategoryResponse> {
+        return try {
+            val response = api.createJobCategory(request)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun updateJob(id: Int, request: CreateJobRequest): Resource<CreateJobsResponse> {
+        return try {
+            val token = getAccessToken(context)
+            val response = api.updateJob(token, id, request)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun deleteJob(id: Int): Resource<CreateJobsResponse> {
+        return try {
+            val token = getAccessToken(context)
+            val response = api.deleteJob(token, id)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun searchJobs(query: String): Resource<List<JobResponse>> {
+        return try {
+            val response = api.searchJobs(query)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun filterJobsByCategory(categoryId: Int): Resource<List<JobResponse>> {
+        return try {
+            val response = api.getJobsByCategory(categoryId)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun filterJobsByBusiness(businessId: Int): Resource<List<JobResponse>> {
+        return try {
+            val response = api.getJobsByBusiness(businessId)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun filterJobsByLocation(latLng: LatLng): Resource<List<JobResponse>> {
+        return try {
+            val response = api.getJobsNearby(latLng.latitude, latLng.longitude)
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    suspend fun filterRecentJobs(): Resource<List<JobResponse>> {
+        return try {
+            val response = api.getRecentJobs()
+            Resource.Success(response)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
     private fun <T>handleException(e: Exception): Resource<T>{
         return if (e is HttpException && (e.hashCode() == 401 || e.hashCode() == 403)) {
             Resource.Error("Authorization failed. Please check your credentials.")
@@ -202,6 +357,4 @@ class KaziHubRepository
             Resource.Error(e.message ?: "An error occurred")
         }
     }
-
-
 }
