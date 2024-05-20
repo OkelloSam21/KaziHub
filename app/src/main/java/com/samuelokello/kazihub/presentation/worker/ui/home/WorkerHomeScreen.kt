a4f2bed484d5a8144a21caaed41a9be53e730400
Samuel Okello <samuelokello929@gmail.com>
2024-05-20 23:43:34 +0300

    enhance WorkerHomeScreen

--- app/src/main/java/com/samuelokello/kazihub/presentation/worker/ui/home/WorkerHomeScreen.kt
+++ app/src/main/java/com/samuelokello/kazihub/presentation/worker/ui/home/WorkerHomeScreen.kt
@@ -1,12 +1,13 @@
 package com.samuelokello.kazihub.presentation.worker.ui.home
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,16 +16,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -33,6 +36,9 @@ import com.samuelokello.kazihub.presentation.shared.components.AppBar
 import com.samuelokello.kazihub.presentation.shared.components.JobCard
 import com.samuelokello.kazihub.presentation.worker.data.HomeViewModel
 import com.samuelokello.kazihub.presentation.worker.data.Job
+import com.samuelokello.kazihub.presentation.worker.data.WorkerHomeScreenUiState
+import com.samuelokello.kazihub.ui.theme.KaziHubTheme
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 /**
@@ -48,54 +54,70 @@ fun WorkerHomeScreen() {
     val state = viewModel.state.collectAsState()
     val scope = rememberCoroutineScope()
 
-    val context = LocalContext.current
+    Surface {
+        KaziHubTheme {
+            WorkerHomeScreenContent(state = state, scope = scope)
+        }
+    }
 
-    Log.e("WorkerHomeScreen", "WorkerHomeScreen: ${state.value.data.size}")
-    Log.e("WorkerHomeScreen", "WorkerHomeScree")
+}
 
-    Scaffold(
-        topBar = {
-            AppBar {
-                scope.launch {
-                    state.value.drawerState
-                }
+@Composable
+fun WorkerHomeScreenContent(
+    state: State<WorkerHomeScreenUiState>,
+    scope: CoroutineScope
+) {
+    Column {
+        AppBar {
+            scope.launch {
+                state.value.drawerState
             }
-        },
-        modifier = Modifier.padding(bottom = 32.dp),
-    ) {
+        }
         Column(
             modifier = Modifier
                 .padding(top = 32.dp, start = 16.dp, end = 16.dp)
-                .fillMaxWidth(),
-            ) {
-            Column {
+                .fillMaxWidth()
+//                .pullRefresh(),
+        ) {
+            if (state.value.isLoading) {
+                Box(modifier = Modifier.fillMaxSize()){
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+            } else {
                 Column {
-                    Row {
-                        Text(
-                            text = "Popular Jobs",
-                            style = MaterialTheme.typography.bodyLarge,
-                            modifier = Modifier.padding(bottom = 16.dp)
-                        )
+                    Column {
+                        Row {
+                            Text(
+                                text = "Popular Jobs",
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.padding(bottom = 16.dp)
+                            )
 
-                        Spacer(modifier = Modifier.weight(2f))
-                        TextButton(onClick = { /*TODO*/ }) {
-                            Text(text = "View All")
+                            Spacer(modifier = Modifier.weight(2f))
+                            TextButton(onClick = { /*TODO*/ }) {
+                                Text(text = "View All")
+                            }
                         }
                     }
-                }
-
-                LazyRow {
-                    items(state.value.data.size) { job ->
-                        state.value.data[job]?.let { it1 -> JobCard(job = it1) }
+                    Column {
+                        Text(text = "")
+                        LazyRow {
+                            items(state.value.data.size) { job ->
+                                state.value.data[job]?.let { it1 -> JobCard(job = it1) }
+                            }
+                        }
                     }
-                }
 
-                Spacer(modifier = Modifier.height(16.dp))
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Column {
+                        Text(text = "Recent Posts")
+                        LazyColumn {
 
-                LazyColumn {
-                    items(state.value.data) { job ->
-                        if (job != null) {
-                            RecentPost(job)
+                            items(state.value.data) { job ->
+                                if (job != null) {
+                                    RecentPost(job)
+                                }
+                            }
                         }
                     }
                 }
