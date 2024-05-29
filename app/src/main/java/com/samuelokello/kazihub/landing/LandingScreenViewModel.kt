2fb8ce345766eb252e718c68bb60d03160a16e3e
Samuel Okello <samuelokello929@gmail.com>
2024-05-29 17:46:30 +0300

    enhancement

--- app/src/main/java/com/samuelokello/kazihub/landing/LandingScreenViewModel.kt
+++ app/src/main/java/com/samuelokello/kazihub/landing/LandingScreenViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.viewModelScope
 import com.samuelokello.kazihub.domain.repositpry.AuthRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -31,22 +30,22 @@ class LandingScreenViewModel @Inject constructor(val repository: AuthRepository)
 
     private fun checkNavigationDestination() {
         viewModelScope.launch {
-            val userHasOnBoarded = repository.userHasOnBoarded.first()
-            if (userHasOnBoarded.not()) {
-                _state.update { it.copy(destination = LandingScreenState.Destination.USER_TYPE) }
-            }
-
-            val hasSignedIn = repository.userHasSignedIn.first()
-            if (hasSignedIn.not()) {
-                _state.update { it.copy(destination = LandingScreenState.Destination.SIGN_IN) }
-                return@launch
-            }
-
-            val hasSignedUp = repository.userHasSignedUp.first()
-            if (hasSignedUp.not()) {
-                _state.update { it.copy(destination = LandingScreenState.Destination.SIGN_UP) }
-                return@launch
-            }
+//            val userHasOnBoarded = repository.userHasOnBoarded.first()
+//            if (userHasOnBoarded.not()) {
+//                _state.update { it.copy(destination = LandingScreenState.Destination.USER_TYPE) }
+//            }
+//
+//            val hasSignedIn = repository.userHasSignedIn.first()
+//            if (hasSignedIn.not()) {
+//                _state.update { it.copy(destination = LandingScreenState.Destination.SIGN_IN) }
+//                return@launch
+//            }
+//
+//            val hasSignedUp = repository.userHasSignedUp.first()
+//            if (hasSignedUp.not()) {
+//                _state.update { it.copy(destination = LandingScreenState.Destination.SIGN_UP) }
+//                return@launch
+//            }
 
             _state.update { it.copy(destination = LandingScreenState.Destination.HOME) }
         }
