a05944de02185f037db861d93d703e53b249f798
Samuel Okello <samuelokello929@gmail.com>
2024-05-29 16:11:31 +0300

    enhancement determine user landing screen

--- app/src/main/java/com/samuelokello/kazihub/domain/repositpry/KaziHubRepository.kt
+++ app/src/main/java/com/samuelokello/kazihub/domain/repositpry/KaziHubRepository.kt
@@ -10,8 +10,21 @@ import com.samuelokello.kazihub.domain.model.sign_up.SignUpResponse
 import com.samuelokello.kazihub.domain.model.worker.WorkerProfileRequest
 import com.samuelokello.kazihub.domain.model.worker.WorkerProfileResponse
 import com.samuelokello.kazihub.utils.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface KaziHubRepository {
+    val userHasSignedUp: Flow<Boolean>
+
+    val userHasSignedIn: Flow<Boolean>
+
+    val userHasOnBoarded: Flow<Boolean>
+    suspend fun refreshToken(token: String): Resource<String>
+
+    suspend fun validation(validationCode: String): Flow<Resource<String>>
+    fun readOnBoardingState(): Flow<Boolean>
+
+    suspend fun setUserHasOnboarded()
+
     suspend fun signUp(signUpRequest: SignUpRequest): Resource<SignUpResponse>
     suspend fun signIn(signInRequest: SignInRequest): Resource<SignInResponse>
     suspend fun createBusinessProfile(request: BusinessProfileRequest): Resource<BusinessProfileResponse>
--- app/src/main/java/com/samuelokello/kazihub/presentation/shared/auth/sign_up/SignUpScreen.kt
+++ app/src/main/java/com/samuelokello/kazihub/presentation/shared/auth/sign_up/SignUpScreen.kt
@@ -49,9 +49,9 @@ import com.samuelokello.kazihub.R
 import com.samuelokello.kazihub.presentation.common.HandleError
 import com.samuelokello.kazihub.presentation.common.HandleLoading
 import com.samuelokello.kazihub.presentation.common.HandleSuccess
+import com.samuelokello.kazihub.presentation.destinations.SignInScreenDestination
 import com.samuelokello.kazihub.presentation.shared.components.CustomButton
 import com.samuelokello.kazihub.presentation.shared.components.EditTextField
-import com.samuelokello.kazihub.presentation.shared.destinations.SignInScreenDestination
 import com.samuelokello.kazihub.ui.theme.KaziHubTheme
 import com.samuelokello.kazihub.utils.UserRole
 
