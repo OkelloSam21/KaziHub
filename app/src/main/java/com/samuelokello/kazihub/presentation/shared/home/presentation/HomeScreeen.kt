384602f0cfd898d9cf4ee35806837739bd3d67fa
Samuel Okello <samuelokello929@gmail.com>
2024-05-31 11:05:57 +0100

    enhancement

--- app/src/main/java/com/samuelokello/kazihub/presentation/shared/home/presentation/HomeScreeen.kt
+++ app/src/main/java/com/samuelokello/kazihub/presentation/shared/home/presentation/HomeScreeen.kt
@@ -3,7 +3,7 @@ package com.samuelokello.kazihub.presentation.shared.home.presentation
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import com.samuelokello.kazihub.presentation.business.BusinessHomeScreen
+import com.samuelokello.kazihub.presentation.business.Home.BusinessHomeScreen
 import com.samuelokello.kazihub.presentation.worker.ui.home.WorkerHomeScreen
 import com.samuelokello.kazihub.utils.UserRole
 
