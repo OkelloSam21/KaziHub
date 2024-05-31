0bdb40ada9b8200be42c3b9dfd90f1c8fc942e1f
Samuel Okello <samuelokello929@gmail.com>
2024-05-31 17:28:38 +0100

    enhancement create line graph

--- app/src/main/java/com/samuelokello/kazihub/presentation/business/Home/components/LineGraph.kt
+++ app/src/main/java/com/samuelokello/kazihub/presentation/business/Home/components/LineGraph.kt
@@ -11,6 +11,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import com.samuelokello.kazihub.presentation.worker.data.Job
 
@@ -42,6 +45,7 @@ fun LineGraph(jobs: List<Job>) {
             modifier = Modifier
                 .size(graphWidth, graphHeight)
                 .padding(padding)
+                .align(Alignment.CenterHorizontally)
         ) {
             val paddingPx = padding.toPx()
             val widthPerPoint =
@@ -60,6 +64,56 @@ fun LineGraph(jobs: List<Job>) {
                 Offset(paddingPx, size.height - paddingPx),
                 Offset(size.width - paddingPx, size.height - paddingPx)
             )
+
+
+            // draw y axis labels
+            val yAxisLabelInterval = (maxValue - minValue) / 5
+            for (i in 0..5) {
+                val yValue = (minValue + i * yAxisLabelInterval).toString()
+                val yOffset = size.height - paddingPx - i * (size.height - paddingPx * 2) / 5
+
+                drawContext.canvas.nativeCanvas.drawText(
+                    yValue,
+                    0f - 20, // Subtract a constant value to move the labels to the left
+                    yOffset,
+                    Paint().asFrameworkPaint().apply {
+                        color = Color.Gray.toArgb()
+                        textSize = 24f
+                    }
+                )
+            }
+
+            // draw x axis labels
+            val xAxisLabelInterval = if (jobs.size > 1) jobs.size / 5 else 1
+            for (i in 0..5) {
+                val xValue = (minValue + i * xAxisLabelInterval).toString()
+                val xOffset = paddingPx + i * (size.width - paddingPx * 2) / 5
+
+                drawContext.canvas.nativeCanvas.drawText(
+                    xValue,
+                    xOffset,
+                    size.height - paddingPx + 30, // Add a constant value to move the labels down
+                    Paint().asFrameworkPaint().apply {
+                        color = Color.Gray.toArgb()
+                        textSize = 24f
+                    }
+                )
+            }
+
+            if (jobs.isEmpty()) {
+                drawContext.canvas.nativeCanvas.drawText(
+                    "No amount spent",
+                    size.width / 2,
+                    size.height / 2,
+                    Paint().asFrameworkPaint().apply {
+                        color = android.graphics.Color.BLACK
+                        textSize = 24f
+                        textAlign = android.graphics.Paint.Align.CENTER
+                        style = android.graphics.Paint.Style.FILL
+                    }
+                )
+                return@Canvas
+            }
 
             if (jobs.isNotEmpty()) {
                 for (i in 0 until jobs.size - 1) {
@@ -90,7 +144,6 @@ fun LineGraph(jobs: List<Job>) {
                 // if jobs is empty add text place holder inside the graph
 
             }
-
 
         }
 
