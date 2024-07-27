621d4f03f2295bf5f22f40cf36c66ef61880af13
OkelloSam <samuelokello929@gmail.com>
2024-07-27 08:49:05 +0300

    feat: add check profile functionality
    This commit adds a new method `checkProfile()` to the `AuthRepository` interface to check if a user profile exists.
     This functionality will be used to determine if a user needs to complete their profile after signing up.

--- app/src/main/java/com/samuelokello/kazihub/domain/repositpry/AuthRepository.kt
+++ app/src/main/java/com/samuelokello/kazihub/domain/repositpry/AuthRepository.kt
@@ -2,11 +2,12 @@ package com.samuelokello.kazihub.domain.repositpry
 
 import com.samuelokello.kazihub.data.model.sign_in.SignInRequest
 import com.samuelokello.kazihub.data.model.sign_in.SignInResponse
-import com.samuelokello.kazihub.domain.model.sign_up.SignUpRequest
-import com.samuelokello.kazihub.domain.model.sign_up.SignUpResponse
+import com.samuelokello.kazihub.domain.model.shared.auth.sign_up.SignUpRequest
+import com.samuelokello.kazihub.domain.model.shared.auth.sign_up.SignUpResponse
 import com.samuelokello.kazihub.utils.Resource
 
 interface AuthRepository {
     suspend fun signUp(signUpRequest: SignUpRequest): Resource<SignUpResponse>
     suspend fun signIn(signInRequest: SignInRequest): Resource<SignInResponse>
+    suspend fun checkProfile(): Resource<Boolean>
 }
