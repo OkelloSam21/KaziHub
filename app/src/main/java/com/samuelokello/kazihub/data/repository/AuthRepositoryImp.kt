7f6c6c838ae9ecdb08c14ce86f3784f2bcae0370
OkelloSam <samuelokello929@gmail.com>
2024-07-28 15:07:56 +0300

    refactor: simplify API calls with safeApiCall utility
    This commit simplifies API calls in AuthRepositoryImpl by utilizing the safeApiCall utility function.
    
    This change improves error handling and reduces code duplication.

--- app/src/main/java/com/samuelokello/kazihub/data/repository/AuthRepositoryImp.kt
+++ app/src/main/java/com/samuelokello/kazihub/data/repository/AuthRepositoryImp.kt
@@ -1,17 +1,15 @@
 package com.samuelokello.kazihub.data.repository
 
-import android.util.Log
 import com.samuelokello.kazihub.data.model.profile.ProfileResponse
 import com.samuelokello.kazihub.data.model.sign_in.SignInRequest
 import com.samuelokello.kazihub.data.model.sign_in.SignInResponse
 import com.samuelokello.kazihub.data.remote.KaziHubApi
-import com.samuelokello.kazihub.di.TokenManager
 import com.samuelokello.kazihub.domain.model.shared.auth.sign_up.SignUpRequest
 import com.samuelokello.kazihub.domain.model.shared.auth.sign_up.SignUpResponse
 import com.samuelokello.kazihub.domain.repositpry.AuthRepository
 import com.samuelokello.kazihub.utils.Resource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.samuelokello.kazihub.utils.TokenManager
+import com.samuelokello.kazihub.utils.safeApiCall
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
@@ -20,56 +18,23 @@ class AuthRepositoryImpl @Inject constructor(
 ) : AuthRepository {
 
     override suspend fun signUp(signUpRequest: SignUpRequest): Resource<SignUpResponse> =
-        withContext(Dispatchers.IO) {
-            try {
-                val response = api.signUp(signUpRequest)
-                Log.d(TAG, "signUp: $response")
-                Resource.Success(response)
-            } catch (e: Exception) {
-                Log.e(TAG, "signUp error: ${e.message}", e)
-                Resource.Error(e.message ?: "An unexpected error occurred")
-            }
+        safeApiCall {
+            api.signUp(signUpRequest)
         }
 
     override suspend fun signIn(signInRequest: SignInRequest): Resource<SignInResponse> =
-        withContext(Dispatchers.IO) {
-            try {
-                val response = api.signIn(signInRequest)
-                response.data?.accessToken?.let { tokenManager.setToken(it) }
-                Log.d(TAG, "signIn: ${response.data?.accessToken}")
-                Resource.Success(response)
-            } catch (e: Exception) {
-                Log.e(TAG, "signIn error: ${e.message}", e)
-                Resource.Error(e.message ?: "An unexpected error occurred")
-            }
+        safeApiCall {
+            api.signIn(signInRequest)
+        }.also { signInResponse ->
+            tokenManager.setToken(signInResponse.data?.data?.accessToken!!, signInResponse.data.data.refreshToken!!)
         }
+
 
     override suspend fun getCurrentUser(): Resource<ProfileResponse> =
-        withContext(Dispatchers.IO) {
-            try {
-                val response = api.getCurrentUser()
-                Log.d(TAG, "getCurrentUser: $response")
-                Resource.Success(response)
-            } catch (e: Exception) {
-                Log.e(TAG, "getCurrentUser error: ${e.message}", e)
-                Resource.Error(e.message ?: "An unexpected error occurred")
-            }
-        }
+        safeApiCall { api.getCurrentUser() }
+
 
     override suspend fun refreshToken(): Resource<SignInResponse> =
-        withContext(Dispatchers.IO) {
-            try {
-                val response = api.refreshToken(tokenManager.getToken())
-                response.data?.accessToken?.let { tokenManager.setToken(it) }
-                Log.d(TAG, "refreshToken: ${response.data?.accessToken}")
-                Resource.Success(response)
-            } catch (e: Exception) {
-                Log.e(TAG, "refreshToken error: ${e.message}", e)
-                Resource.Error(e.message ?: "An unexpected error occurred")
-            }
-        }
+        safeApiCall { api.refreshToken(tokenManager.getAccessToken()) }
 
-    companion object {
-        private const val TAG = "AuthRepositoryImpl"
-    }
 }
