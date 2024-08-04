9f7d8b26116f307c6859a9c6e439d3db3d90c12c
OkelloSam <samuelokello929@gmail.com>
2024-08-04 19:57:05 +0300

    feat: add auth check on home screen
    This commit adds an authentication check on the home screen to ensure that only logged-in users can access it.
    
    - A new AuthViewModel is introduced to handle the authentication logic.
    - A LaunchedEffect is used to check the login status and navigate to the UserTypeScreen if the user is not logged in.
    - The navigation is set up to pop up to the HomeScreenDestination, ensuring a clean transition.

--- app/src/main/java/com/samuelokello/kazihub/presentation/shared/home/presentation/HomeScreeen.kt
+++ app/src/main/java/com/samuelokello/kazihub/presentation/shared/home/presentation/HomeScreeen.kt
@@ -1,11 +1,17 @@
 package com.samuelokello.kazihub.presentation.shared.home.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.samuelokello.kazihub.presentation.business.Home.BusinessHomeScreen
+import com.samuelokello.kazihub.presentation.destinations.HomeScreenDestination
+import com.samuelokello.kazihub.presentation.destinations.UserTYpeScreenDestination
 import com.samuelokello.kazihub.presentation.worker.ui.home.WorkerHomeScreen
 import com.samuelokello.kazihub.utils.UserRole
+import javax.inject.Inject
 
 /***
  * A composable function that displays the home screen of the app.
@@ -13,9 +19,30 @@ import com.samuelokello.kazihub.utils.UserRole
  * */
 @Destination
 @Composable
-fun HomeScreen(userType: UserRole = UserRole.BUSINESS, navigator: DestinationsNavigator) {
+fun HomeScreen(
+    userType: UserRole,
+    navigator: DestinationsNavigator,
+    authViewModel: AuthViewModel = hiltViewModel()
+) {
+
+    LaunchedEffect(key1 = true) {
+        if (!authViewModel.isUserLoggedIn()) {
+            navigator.navigate(UserTYpeScreenDestination) {
+                popUpTo(HomeScreenDestination.route) { inclusive = true }
+            }
+        }
+    }
+
     when (userType) {
         UserRole.BUSINESS -> BusinessHomeScreen()
         UserRole.WORKER -> WorkerHomeScreen(navigator = navigator)
     }
+}
+
+class AuthViewModel @Inject constructor(): ViewModel() {
+    fun isUserLoggedIn(): Boolean {
+        return true
+    }
+
+
 }
