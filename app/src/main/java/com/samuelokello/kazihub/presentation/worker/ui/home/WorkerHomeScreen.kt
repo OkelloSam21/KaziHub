c65aefa5cb1d11a5452d2796152e25b2d24350b7
OkelloSam <samuelokello929@gmail.com>
2024-08-04 23:14:05 +0300

    feat: improve worker home screen UI
    - Refactor WorkerHomeScreenContent to use JobSection composable for better code organization.
    - Add NoJobsMessage component
     to display a message when no jobs are available.
    - Update JobCard to handle click events and navigate to JobDetailsScreen.
    - Remove unused code and optimize imports.

--- app/src/main/java/com/samuelokello/kazihub/presentation/worker/ui/home/WorkerHomeScreen.kt
+++ app/src/main/java/com/samuelokello/kazihub/presentation/worker/ui/home/WorkerHomeScreen.kt
@@ -1,7 +1,6 @@
 package com.samuelokello.kazihub.presentation.worker.ui.home
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
@@ -32,24 +30,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.samuelokello.kazihub.R
 import com.samuelokello.kazihub.domain.model.job.WorkerHomeScreenUiState
+import com.samuelokello.kazihub.domain.model.job.data
 import com.samuelokello.kazihub.presentation.common.components.AppBar
 import com.samuelokello.kazihub.presentation.common.components.JobCard
+import com.samuelokello.kazihub.presentation.common.components.NoJobsMessage
+import com.samuelokello.kazihub.presentation.destinations.JobDetailsScreenDestination
 import com.samuelokello.kazihub.presentation.worker.data.HomeViewModel
-import com.samuelokello.kazihub.domain.model.job.Job
 import com.samuelokello.kazihub.presentation.worker.state.WorkerHomeScreenUiEvent
 import com.samuelokello.kazihub.ui.theme.KaziHubTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-
-/**
- * Worker Home Screen
- * A composable function that displays the worker home screen
- * It contains a search bar, popular data, and recent posts
- * */
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -57,7 +53,6 @@ fun WorkerHomeScreen(
     navigator: DestinationsNavigator,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-
     val homeUiState by viewModel.state.collectAsState()
     val scope = rememberCoroutineScope()
 
@@ -69,31 +64,11 @@ fun WorkerHomeScreen(
                 event = { event ->
                     when (event) {
                         is WorkerHomeScreenUiEvent.JobSelected -> {
-
+                            navigator.navigate(JobDetailsScreenDestination)
                         }
-
-                        is WorkerHomeScreenUiEvent.NavigateToJobDetails -> {
-
-                        }
-
-                        WorkerHomeScreenUiEvent.OnSHowAllClick -> {
-//                            navigator.navigate()
-                        }
-
-                        WorkerHomeScreenUiEvent.OnSearchClick -> {
-//                            navigator.navigate()
-                        }
-
-                        WorkerHomeScreenUiEvent.OnViewAllClick -> {
-                            TODO()
-                        }
-
-                        WorkerHomeScreenUiEvent.OpenDrawer -> {
-
-                        }
-
-                        is WorkerHomeScreenUiEvent.SearchQueryChanged -> TODO()
-                        WorkerHomeScreenUiEvent.OnPullToRefresh -> {
+                        is WorkerHomeScreenUiEvent.OnViewAllClick -> { }
+                        is WorkerHomeScreenUiEvent.OpenDrawer ->{}
+                        is WorkerHomeScreenUiEvent.OnPullToRefresh -> {
                             viewModel.refreshAllData()
                         }
                     }
@@ -101,7 +76,6 @@ fun WorkerHomeScreen(
             )
         }
     }
-
 }
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -111,108 +85,91 @@ fun WorkerHomeScreenContent(
     scope: CoroutineScope,
     event: (WorkerHomeScreenUiEvent) -> Unit
 ) {
-    val recentJobs = state.recentJobs
-    val popularJobs = state.nearByJobs
-    val isRefreshing = state.isRefreshing
     val pullRefreshState = rememberPullRefreshState(
-        refreshing = false,
+        refreshing = state.isRefreshing,
         onRefresh = { event(WorkerHomeScreenUiEvent.OnPullToRefresh) }
     )
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .pullRefresh(state = pullRefreshState)
     ) {
         Column(modifier = Modifier.fillMaxSize()) {
-            AppBar {
-                scope.launch {
-                    state.drawerState
-                }
-            }
+            AppBar { scope.launch { state.drawerState } }
 
             Column(
                 modifier = Modifier
-                    .padding(top = 32.dp, start = 16.dp, end = 16.dp)
+                    .padding(16.dp)
                     .fillMaxWidth()
             ) {
-                if (state.isLoading && state.nearByJobs.isEmpty()) {
+                if (state.isLoading) {
                     Box(modifier = Modifier.fillMaxSize()) {
                         CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
                     }
                 } else {
-                    Column {
-
-                        Column {
-                            Row {
-                                Text(
-                                    text = "Near By Jobs",
-                                    style = MaterialTheme.typography.bodyLarge,
-                                    modifier = Modifier.padding(bottom = 16.dp)
-                                )
-
-                                Spacer(modifier = Modifier.weight(2f))
-                                TextButton(onClick = { event(WorkerHomeScreenUiEvent.OnViewAllClick) }) {
-                                    Text(text = "View All")
-                                }
-                            }
-
-                            Log.d(
-                                "WorkerHomeScreenContent",
-                                "popularJobs size: ${popularJobs.size}"
-                            )
-
-                            LazyRow {
-                                items(popularJobs) { job ->
-                                    if (job != null) {
-                                        JobCard(job = job)
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    JobSection(
+                        title = "Near By Jobs",
+                        jobs = state.nearByJobs,
+                        onViewAllClick = { event(WorkerHomeScreenUiEvent.OnViewAllClick) },
+                        onJobClick = { job -> event(WorkerHomeScreenUiEvent.JobSelected(job)) }
+                    )
                     Spacer(modifier = Modifier.height(16.dp))
-                    Column {
-                        Row {
-                            Text(text = "Recent Jobs")
-                            Spacer(modifier = Modifier.weight(2f))
-                            TextButton(onClick = { event(WorkerHomeScreenUiEvent.OnViewAllClick) }) {
-                                Text(text = "Show All")
-                            }
-                        }
-
-                        Log.d("WorkerHomeScreenContent", "recentJobs size: ${recentJobs.size}")
-                        LazyColumn {
-
-                            items(recentJobs) { job ->
-                                RecentPost(
-                                    job = job,
-                                    onEvent = event
-                                )
-                            }
-                        }
-                    }
-
+                    JobSection(
+                        title = "Recent Jobs",
+                        jobs = state.recentJobs,
+                        onViewAllClick = { event(WorkerHomeScreenUiEvent.OnViewAllClick) },
+                        onJobClick = { job -> event(WorkerHomeScreenUiEvent.JobSelected(job)) }
+                    )
                 }
             }
         }
 
         PullRefreshIndicator(
-            refreshing = isRefreshing,
+            refreshing = state.isRefreshing,
             state = pullRefreshState,
             Modifier.align(Alignment.TopCenter)
         )
     }
 }
 
+@Composable
+fun JobSection(
+    title: String,
+    jobs: List<data?>,
+    onViewAllClick: () -> Unit,
+    onJobClick: (data) -> Unit
+) {
+    Column {
+        Row {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            TextButton(onClick = onViewAllClick) {
+                Text(text = "View All")
+            }
+        }
 
-/**
- * Recent Post
- * A composable function that displays a recent post
- * */
+        if (jobs.isEmpty()) {
+            NoJobsMessage(R.string.no_jobs_message)
+        } else {
+            LazyRow {
+                items(jobs) { job ->
+                    job?.let {
+                        JobCard(job = it) { onJobClick(it) }
+                    }
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun RecentPost(
-    job: Job,
+    job: data,
     onEvent: (WorkerHomeScreenUiEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -224,35 +181,31 @@ fun RecentPost(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface,
             contentColor = MaterialTheme.colorScheme.onSurface,
-        ),
-        content = {
-            Column(
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(32.dp)
-                ) {
-//                    AsyncImage(
-//                        model = ImageRequest.Builder(LocalContext.current)
-//                            .data(job.imageUrl)
-//                            .crossfade(true)
-//                            .build(),
-//                        placeholder = painterResource(id = R.drawable.icons8_google_48),
-//                        error = painterResource(id = R.drawable.icons8_google_48),
-//                        contentScale = ContentScale.Crop,
-//                        contentDescription = null,
-//                        modifier = modifier
-//                            .size(40.dp)
-//                    )
-                    Column {
-                        Text(text = job.title.toString())
-                        Text(text = job.location.toString())
-                    }
-
-                    Text(text = "ksh ${job.budget}")
-                }
+        )
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(32.dp)
+        ) {
+            Column {
+                Text(text = job.title ?: "")
+                Text(text = job.location ?: "")
             }
+            Text(text = "ksh ${job.budget}")
         }
-    )
+    }
 }
 
+@Preview(showBackground = true)
+@Composable
+private fun WorkerHomePreview() {
+    Surface {
+        KaziHubTheme {
+            WorkerHomeScreenContent(
+                state = WorkerHomeScreenUiState(),
+                scope = rememberCoroutineScope(),
+                event = {}
+            )
+        }
+    }
+}
