c971ee8968efb4c370dae3f6de2ba1164a5823ce
OkelloSam <samuelokello929@gmail.com>
2024-08-04 19:53:59 +0300

    chore: update dependencies
    This commit updates various dependencies to their latest versions, including:
    
    - AndroidX libraries (lifecycle, activity, compose, material)
    
    - Google Play Services (maps, location, places)
    - Firebase (auth, database, storage)
    - Kotlin Coroutines
    - Hilt
    - Retrofit
    - OkHttp
    - Mockito
    - Coil
    - Accompanist
    
    This ensures the app uses the most up-to-date and secure
     versions of these libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,47 +57,47 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material:material:1.6.7")
+    implementation("androidx.compose.material:material:1.6.8")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("androidx.compose.material3:material3-android:1.2.1")
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
-    implementation("com.google.android.gms:play-services-location:21.2.0")
-    implementation("com.google.android.libraries.places:places:3.4.0")
-    implementation("androidx.test.ext:junit-ktx:1.1.5")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.android.libraries.places:places:3.5.0")
+    implementation("androidx.test.ext:junit-ktx:1.2.1")
     implementation("androidx.datastore:datastore-core-android:1.1.1")
     implementation("androidx.datastore:datastore-preferences-core-jvm:1.1.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //extended icons
-    implementation("androidx.compose.material:material-icons-extended-android:1.6.7")
+    implementation("androidx.compose.material:material-icons-extended-android:1.6.8")
 
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
 
     // Compose Destinations
     implementation("io.github.raamcosta.compose-destinations:animations-core:1.10.1")
     ksp("io.github.raamcosta.compose-destinations:ksp:1.10.1")
 
     // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 
     // Coroutine Lifecycle Scopes
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
 
     // Dagger - Hilt
     implementation ("com.google.dagger:hilt-android:2.51")
@@ -114,7 +114,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.10.0")
 
     implementation ("com.google.firebase:firebase-auth-ktx:23.0.0")
-    implementation ("com.google.android.gms:play-services-auth:21.1.1")
+    implementation ("com.google.android.gms:play-services-auth:21.2.0")
 
     //Okhttp
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
@@ -124,10 +124,10 @@ dependencies {
     implementation("org.mockito:mockito-core:4.0.0")
 
     // Google Sign In SDK
-    implementation("com.google.android.gms:play-services-auth:21.1.1")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 
     // Firebase SDK
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-database-ktx")
     implementation("com.google.firebase:firebase-storage-ktx")
     implementation("com.google.firebase:firebase-auth-ktx")
@@ -140,4 +140,6 @@ dependencies {
 
     //swipe refresh
 //    implementation("com.google.accompanist:accompanist-swiperefresh: 0.34.0")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
